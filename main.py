a0628536f3e21439b6e21b47ceae83c915e13446
al. <nonfict@proton.me>
2025-08-14 21:01:17 +0900

    e

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from selenium.webdriver.chrome.options import Options
 import time
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-import random
+import numpy as np
 
 #options = Options()
 """options.add_argument('--user-data-dir=/users/al/chrome3')
@@ -17,7 +17,7 @@ driver = webdriver.Chrome()#options=options)
 
 driver.get("https://www.minecraft.net/ja-jp/msaprofile/mygames/editprofile")
 
-time.sleep(2.5)
+time.sleep(3.5)
 
 print(driver.find_element(By.XPATH, "//a[contains(text(), 'サインイン')][@aria-label='Microsoft でサインインする']"))
 driver.find_element(By.XPATH, "//a[contains(text(), 'サインイン')][@aria-label='Microsoft でサインインする']").click()
@@ -30,25 +30,26 @@ driver.find_element(By.XPATH, "//a[contains(text(), 'サインイン')][@aria-la
 
 time.sleep(2)
 
-driver.find_element(By.XPATH, "//input[@type='email']").send_keys("\n") #mail
+driver.find_element(By.XPATH, "//input[@type='email']").send_keys("ytakada_dc@yahoo.co.jp\n")
 driver.find_element(By.XPATH, "//input[@type='email']").send_keys(Keys.ENTER)
-time.sleep(0.5)
+time.sleep(1)
 driver.find_element(By.XPATH, "//span[contains(text(), 'その他のサインイン方法')]").click()
-time.sleep(0.5)
+time.sleep(1)
 driver.find_element(By.XPATH, "//span[contains(text(), 'パスワードを使用する')]").click()
-time.sleep(0.5) 
-driver.find_element(By.XPATH, "//input[@type='password']").send_keys("") #password
+time.sleep(1)
+driver.find_element(By.XPATH, "//input[@type='password']").send_keys("Gatag0to")
 driver.find_element(By.XPATH, "//input[@type='password']").send_keys(Keys.ENTER)
-time.sleep(0.5)
+time.sleep(1)
 driver.find_element(By.XPATH, "//button[contains(text(), 'はい')]").click()
 time.sleep(5)
 
 for j in range(10000000000):
     driver.get("https://www.minecraft.net/ja-jp/msaprofile/mygames/editprofile")
-    time.sleep(2)
+    time.sleep(0.5)
     driver.find_element(By.XPATH, "//input[@class='MCR_TextInput_Input']").send_keys("KAMI_kousei")
     driver.find_element(By.XPATH, "//span[contains(text(), 'プロフィール名を変更')]").click()
-    time.sleep(random.randint(0, 20))
+    time.sleep(2 ** np.random.uniform(-5, 4))
+    time.sleep(0.5)
 #html_source = driver.page_source
 #print(html_source.split("プロフィール名"))
 
